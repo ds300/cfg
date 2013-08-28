dcde4abdda57e4dedaaee4f1978f7cc72a47d42d
David Sheldrick <djsheldrick@gmail.com>
2013-08-28 20:23:40 +0100

    starting from scratch

--- src/cfg/types.clj
+++ src/cfg/types.clj
@@ -1,83 +1,32 @@
-(ns cfg.types
-  "Here be all the types I am supplying out of the box."
-  (:require [cfg.types.core :refer [paramtype resolve-typemap]]
-            [clojure.edn :as edn]
-            :reload-all))
+(ns cfg.types)
 
-(defmacro defparamtype [nm & args]
-  `(def ~nm (paramtype ~@args)))
+(defprotocol ProtoType
+  (validate [me value])
+  (parse [me value])
+  (get-default [me]))
 
-(defn in-range [a b]
-  (fn [x]
-    (and (>= x a) (<= x b))))
+(defprotocol AbstractType
+  (compose [me other])
+  (finalize [me]))
 
-(defn one-of [& ks]
-  (paramtype [(into #{} ks)]
-    :description (str "one of the following: "
-                   (apply str (interpose ", " ks)))))
+(defrecord AbstractMap [properties])
+(defrecord AbstractSeq [properties])
+(defrecord AbstractVal [properties])
 
-(defparamtype bool
-  "Boolean flag"
-  :take 0
-  :validate (fn [b] (instance? Boolean b)))
+(extend-type AbstractMap
+  AbstractType
+  (compose [me other]
+    (if (= AbstractMap (type other))
+      (->AbstractMap (apply merge (map :properties [me other]))))))
 
-(defparamtype integral
-  "Integer"
-  [integer?]
-  :cli-parse edn/read-string)
+(defn get-mixins-and-docstring [args]
+  (let [[[mixins] remaining] (split-with vector? args)
+        [[description] remaining] (split-with string? remaining)]
+    [(or mixins []) description remaining]))
 
-(defparamtype int64 [integral]
-  :parse long)
+(defn valtype [& args]
+  (let [[mixins description remaining] (get-mixins-and-docstring args)]
+    ))
 
-(defparamtype int32 [integral]
-  :parse int)
-
-(defparamtype decimal
-  "Decimal value"
-  [decimal?]
-  :cli-parse edn/read-string)
-
-(defparamtype float64 [decimal]
-  :parse double)
-
-(defparamtype float32 [decimal]
-  "Floating Point Number"
-  :parse float)
-
-(defparamtype required
-  :required true)
-
-(defparamtype optional
-  :requried false)
-
-(defparamtype sequential
-  :seq? true
-  :seq-parse vec)
-
-(defparamtype multi [sequential]
-  :take -1)
-
-(defparamtype tuple [sequential]
-  :take 2
-  :seq-validate #(= 2 (count %)))
-
-(defparamtype triple [sequential]
-  :take 3
-  :seq-validate #(= 3 (count %)))
-
-(defparamtype url
-  "URL"
-  :parse #(java.net.URL. %))
-
-(defparamtype keyw [keyword?]
-  "clojure keyword"
-  :parse keyword
-  :cli-parse keyword)
-
-(defparamtype string
-  "String"
-  :add-validator string?)
-
-(defparamtype csv [sequential]
-  "comma-separated values"
-  :cli-seq-parse #(clojure.string/split % #","))
+(valtype [mixins] "description"
+  :keys :values)
